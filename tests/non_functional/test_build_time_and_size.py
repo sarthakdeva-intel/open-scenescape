0364e15d32b9467eadf58b200b53cbfa323d5666
Patryk Iracki <patryk.iracki@intel.com>
2026-07-23 15:47:36 +0200

    Update thersholds of build size&time test. (#1662)

--- tests/non_functional/test_build_time_and_size.py
+++ tests/non_functional/test_build_time_and_size.py
@@ -29,8 +29,8 @@ class ImageBuildRequirements:
 
 IMAGES_REQUIREMENTS = [
   ImageBuildRequirements(name="common-base", make_target="build-common", time_limit_seconds=120, size_limit_megabytes=400.0),
-  ImageBuildRequirements(name="manager", make_target="manager", time_limit_seconds=300, size_limit_megabytes=600.0),
-  ImageBuildRequirements(name="controller", make_target="controller", time_limit_seconds=400, size_limit_megabytes=600.0),
+  ImageBuildRequirements(name="manager", make_target="manager", time_limit_seconds=360, size_limit_megabytes=600.0),
+  ImageBuildRequirements(name="controller", make_target="controller", time_limit_seconds=400, size_limit_megabytes=660.0),
   ImageBuildRequirements(name="autocalibration", make_target="autocalibration", time_limit_seconds=400, size_limit_megabytes=800.0),
   ImageBuildRequirements(name="tracker", make_target="tracker", time_limit_seconds=1500, size_limit_megabytes=40.0),
 ]
